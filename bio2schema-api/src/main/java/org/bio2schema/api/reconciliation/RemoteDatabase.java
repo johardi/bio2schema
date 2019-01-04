8e8bd9142a5442e2b5b679b911648921eb1a415a
Josef Hardi <josef.hardi@gmail.com>
2019-01-03 22:08:46 -0800

    Parse as a JSON object if the error stream contains a JSON string, otherwise use a regular string

--- bio2schema-api/src/main/java/org/bio2schema/api/reconciliation/RemoteDatabase.java
+++ bio2schema-api/src/main/java/org/bio2schema/api/reconciliation/RemoteDatabase.java
@@ -3,12 +3,17 @@ package org.bio2schema.api.reconciliation;
 import static java.lang.String.format;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bio2schema.util.JacksonUtils;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
 
 public abstract class RemoteDatabase implements Database {
 
@@ -52,8 +57,20 @@ public abstract class RemoteDatabase implements Database {
         conn.getResponseMessage());
     final InputStream errorStream = conn.getErrorStream();
     if (errorStream != null) {
-      errorMessage += format("Details:\n%s", JacksonUtils.prettyPrint(errorStream));
+      errorMessage += format("Details:\n%s", getStringFromStream(errorStream));
     }
     throw new IOException(errorMessage);
+  }
+
+  private static String getStringFromStream(InputStream errorStream) throws IOException {
+    final Reader errorReader = new InputStreamReader(errorStream, Charsets.UTF_8);
+    String message = CharStreams.toString(errorReader);
+    try {
+      return JacksonUtils.mapper
+          .writerWithDefaultPrettyPrinter()
+          .writeValueAsString(message);
+    } catch(JsonProcessingException e) { // not a JSON string
+      return message;
+    }
   }
 }
