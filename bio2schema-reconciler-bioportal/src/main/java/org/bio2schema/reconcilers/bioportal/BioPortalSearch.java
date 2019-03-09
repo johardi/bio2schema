79f5789be3332e17235a89dff2295fa1971e4408
Josef Hardi <josef.hardi@gmail.com>
2019-03-09 14:11:19 -0800

    Implement a retry mechanism for 408 and 429 response codes

--- bio2schema-reconciler-bioportal/src/main/java/org/bio2schema/reconcilers/bioportal/BioPortalSearch.java
+++ bio2schema-reconciler-bioportal/src/main/java/org/bio2schema/reconcilers/bioportal/BioPortalSearch.java
@@ -2,17 +2,22 @@ package org.bio2schema.reconcilers.bioportal;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static org.bio2schema.util.JsonPreconditions.checkIfObjectNode;
+import static java.lang.String.format;
 import java.io.IOException;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import org.bio2schema.api.reconciliation.Database;
 import org.bio2schema.api.reconciliation.EntityReconciler;
+import org.bio2schema.api.reconciliation.HttpRequestException;
 import org.bio2schema.api.reconciliation.entitytype.MedicalEntity;
 import org.bio2schema.util.JacksonUtils;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class BioPortalSearch implements EntityReconciler<MedicalEntity> {
+
+  private static final int MAX_RETRY = 3;
 
   private final Database database;
 
@@ -22,16 +27,35 @@ public class BioPortalSearch implements EntityReconciler<MedicalEntity> {
 
   @Override
   public Optional<MedicalEntity> reconcile(String inputString) throws IOException {
-    try {
-      Object outputResult = database.find(inputString);
-      BioPortalArticle article = handleResult(inputString, outputResult);
-      return Optional.ofNullable(article);
-    } catch (IOException e) {
-      String message = String.format(
-          "A problem occurred using reconciler '%s'\n> %s",
-          database.getName(), e.getMessage());
-      throw new IOException(message);
+    String detailError = null;
+    for (int i = 0; i < MAX_RETRY; i++) {
+      try {
+        Object outputResult = database.find(inputString);
+        BioPortalArticle article = handleResult(inputString, outputResult);
+        return Optional.ofNullable(article);
+      } catch (IOException e) {
+        detailError = e.getMessage();
+        if (e instanceof HttpRequestException) {
+          HttpRequestException rex = (HttpRequestException) e;
+          if (rex.getResponseCode() == 408 || rex.getResponseCode() == 429) {
+            try {
+              sleepForSomeSeconds(i+1);
+              continue;
+            } catch (InterruptedException ie) {
+              detailError = ie.getMessage();
+              break;
+            }
+          }
+        }
+        break;
+      }
     }
+    throw new IOException(format("A problem occurred using reconciler '%s'\n> %s",
+        database.getName(), detailError));
+  }
+
+  private static void sleepForSomeSeconds(int second) throws InterruptedException {
+    TimeUnit.SECONDS.sleep(second);
   }
 
   @Nullable
